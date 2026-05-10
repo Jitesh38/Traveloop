19bc8d62373a59488325f646ff6ba47d93d33439
DESKTOP-AMMIMIH\jalay <jalay.jha@aimdek.com>
2026-05-10 16:28:17 +0530

    screen7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import UserProfilePage from './pages/UserProfilePage'
 
 // TODO: replace HomePlaceholder with the actual landing page once built
 function HomePlaceholder() {
@@ -22,6 +23,7 @@ function App() {
         <Route path="/login"    element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/home"     element={<HomePlaceholder />} />
+        <Route path="/profile"  element={<UserProfilePage />} />
       </Routes>
     </BrowserRouter>
   )
